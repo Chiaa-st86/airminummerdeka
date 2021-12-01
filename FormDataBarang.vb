f541e05fdad41a0fc8709277c9d6a4c21eb8bde6
abimanyudp <abimanyu.dp@gmail.com>
2021-12-01 13:55:18 +0700

    tambah form data barang
    ..

---
+++ FormDataBarang.vb
@@ -0,0 +1,5 @@
+﻿Public Class FormDataBarang
+    Private Sub FormDataBarang_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        tampilkanData("SELECT * FROM data_produk", DataGridView1)
+    End Sub
+End Class
